4325332055af8526f3430744b595857298cb8def
Your Name <mkurvan1@email.com>
2023-06-05 20:46:17 +0600

    new

--- 2_leson.py
+++ 2_leson.py
@@ -15,3 +15,16 @@
 #for i in range(amount_coin):
 #     random.shuffle(coins)
  #    print(f'все монеты{coins}')
+ # if int(coins[0]) == 0:
+#         k += 1
+#     if int(coins[0]) == 1:
+#         m += 1
+# print(f'всего монет {m, k}')
+#
+# if m > k:
+#     ans = k
+# else:
+#     ans = m
+#
+# print(f"минимальное количество монет, которые нужно перевернуть{ans}")
+

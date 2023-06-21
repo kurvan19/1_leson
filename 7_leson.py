0bff5cb7e8dd8627e11e18aa61cae053eca77087
Your Name <mkurvan1@email.com>
2023-06-21 23:41:51 +0600

    faels

--- 7_leson.py
+++ 7_leson.py
@@ -9,13 +9,39 @@
 # Ввод:пара-ра-рам рам-пам-папам па-ра-па-дам         Вывод:Парам пам-пам
 
 
-def word(words):
-    str = words.lower().split()
-    f = lambda x: sum(1 for i in x if i in 'аоуыэеёиюя')
-    tmp = f(str[0])
-    if all([f(i) == tmp for i in str]):
-        return 'Парам пам-пам'
-    return 'Пам парам'
+#def word(words):
+   # str = words.lower().split()
+   # f = lambda x: sum(1 for i in x if i in 'аоуыэеёиюя')
+  #  tmp = f(str[0])
+   # if all([f(i) == tmp for i in str]):
+    #    return 'Парам пам-пам'
+   # return 'Пам парам'
 
 
-print(word("пара-ра-рам рам-пам-папам па-ра-па-дам "))
+#print(word("пара-ра-рам рам-пам-папам па-ра-па-дам "))
+
+
+#Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+#которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца.
+#Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
+#Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля).
+#Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например,
+#у операции умножения.
+#Пример:
+#Ввод:`print_operation_table(lambda x, y: x * y) `
+#Вывод:
+#1 2 3 4 5 6
+
+#2 4 6 8 10 12
+#3 6 9 12 15 18
+#4 8 12 16 20 24
+#5 10 15 20 25 30
+#6 12 18 24 30 36
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
+
+print_operation_table(lambda x, y: x * y)
+
